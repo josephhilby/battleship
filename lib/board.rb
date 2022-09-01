63c4bc46eb9cccaf7ac0c1b7d601e7ec7e80f846
Joseph Hilby <joseph.hilby@gmail.com>
2022-09-01 18:28:18 -0500

    Add feature board

--- lib/board.rb
+++ lib/board.rb
@@ -25,27 +25,36 @@ class Board
     @cells.keys.include?(coord)
   end
 
+  def place(ship, coord_array)
+    coord_array.each do |coord|
+      if valid_coordinate?(coord)
+        @cells[coord].place_ship(ship)
+      end
+    end
+  end
+
+  def render(is = false)
+    cell_render = @cells.values.map do |value|
+      value.render(is)
+    end
+    "  1 2 3 4 \n" +
+    "A #{cell_render[0]} #{cell_render[1]} #{cell_render[2]} #{cell_render[3]} \n" +
+    "B #{cell_render[4]} #{cell_render[5]} #{cell_render[6]} #{cell_render[7]} \n" +
+    "C #{cell_render[8]} #{cell_render[9]} #{cell_render[10]} #{cell_render[11]} \n" +
+    "D #{cell_render[12]} #{cell_render[13]} #{cell_render[14]} #{cell_render[15]} \n"
+  end
+
   def valid_placement?(ship, coord_array)
-    binding.pry
     coord_array.sort!
     length_fit = coord_array.length == ship.length
     no_other_ship = coord_array.count {|coord| @cells[coord].empty?} == coord_array.length
     check_coord = coord_array.map {|coord| coord.chars}
-    in_row = check_coord.all? {|nest_arr| check_coord[0][0] == nest_arr[0]} && check_coord.all? {|first, second| first[1].to_i + 1 == second[1]}
-    in_column = check_coord.all? {|nest_arr| check_coord[0][1] == nest_arr[1]} &&
-
-
-
-    if length_fit && no_other_ship
+    in_row = (check_coord.all? {|nest_arr| check_coord[0][0] == nest_arr[0]}) && (check_coord.map {|nest_arr| nest_arr[1]} == (check_coord[0][1]..check_coord[-1][1]).to_a)
+    in_column = (check_coord.all? {|nest_arr| check_coord[0][1] == nest_arr[1]}) && (check_coord.map {|nest_arr| nest_arr[0]} == (check_coord[0][0]..check_coord[-1][0]).to_a)
+    if (in_row || in_column) && length_fit && no_other_ship
+      true
+    else
+      false
+    end
   end
 end
-
-
-# coord are in board, probibly not needed because of valid_coord method
-    # coord_array.find do |coord|
-    #   valid_coordinate?(coord) == false
-    # end
-    #     ord_value_array = coord_array.map {|coord| coord.chars}.map do |coord|
-    #   coord[0].ord
-    # end
-    # ord_value = ord_value_array == ord_value_array.sort
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -28,12 +28,74 @@ RSpec.describe Board do
     expect(board.valid_coordinate?("A22")).to eq(false)
   end
 
-  it "Test 4: can validate ship placement" do
+  it "Test 4: ship can fit" do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
 
     expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
     expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+  end
+
+  it "Test 5: ship coord are consecutive" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(true)
+    # changed expect on line 47 & 49 to true
+    expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(true)
+  end
+
+  it "Test 6: coord are not diagonal" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+  end
+
+  it "Test 7: can pass valid placement" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
+    expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
+  end
+
+  it "Test 8: can place ship" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+
+    cell_1 = board.cells["A1"]
+    expect(cell_1.ship).to eq(cruiser)
+    cell_2 = board.cells["A2"]
+    expect(cell_2.ship).to eq(cruiser)
+    cell_3 = board.cells["A3"]
+    expect(cell_3.ship).to eq(cruiser)
+    expect(cell_3.ship).to eq(cell_2.ship)
+  end
+
+  it "Test 9: no overlaping ships" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(submarine, ["A1", "B1"])).to eq(false)
+  end
+
+  it "Test 10: can render board" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+
+    expect(board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
+    expect(board.render(true)).to eq("  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n")
   end
 end
