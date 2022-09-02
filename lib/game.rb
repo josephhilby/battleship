386c35428da4d44c32e5b794c467c18229192c2e
Max MacGillivray <mary.macgillivray@gmail.com>
2022-09-02 12:49:24 -0700

    WIP wip game.rb

---
+++ lib/game.rb
@@ -0,0 +1,20 @@
+class Game
+  attr_reader :board
+  def initialize(board)
+    @board = board
+    @ship = ship
+  end
+  
+  def start
+  end
+  
+  def setup
+  end
+  
+  def turn
+  end
+  
+  def end
+  end
+  
+end
