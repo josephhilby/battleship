0f7328c8e287afcda3191e476dc27c74e8b1188d
Joseph Hilby <joseph.hilby@gmail.com>
2022-09-03 13:47:54 -0500

    WIP player

---
+++ lib/player.rb
@@ -0,0 +1,34 @@
+class Player
+  #may need attr_accessor for :board
+  attr_reader :cpu, :board, :cruiser, :submarine
+  def initialize(cpu = false)
+    @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+    @cpu = cpu
+    @cpu_moves = @board.cells.keys
+  end
+
+  def cpu_move
+    if @cpu
+      @cpu_moves.shuffle!
+      @cpu_moves.pop
+    end
+  end
+
+  def player_move
+    puts "Enter the coordinate for your shot:"
+    loop do
+      player_move = gets.chomp
+      if @board.valid_coordinate?(player_move)
+        return player_move
+      else
+        puts "Please enter a valid coordinate:"
+      end
+    end
+  end
+
+  def has_lost?
+    @cruiser.health == 0 && @submarine.health == 0
+  end
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,48 @@
+require 'rspec'
+require 'pry'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/player'
+
+RSpec.describe Player do
+  it "Test 1: exists" do
+    player_1 = Player.new
+
+    expect(player_1).to be_a(Player)
+  end
+
+  it "Test 2: has a cruiser, submarine, and board" do
+    player_1 = Player.new
+
+    expect(player_1.cruiser.name).to eq("Cruiser")
+    expect(player_1.submarine.name).to eq("Submarine")
+    expect(player_1.board).to be_a(Board)
+  end
+
+  it "Test 3: can be a human or cpu" do
+    player_1 = Player.new
+    player_cpu = Player.new(true)
+
+    expect(player_1.cpu).to eq(false)
+    expect(player_cpu.cpu).to eq(true)
+  end
+
+  it "Test 4: can make a turn as a cpu" do
+    player_cpu = Player.new(true)
+    move = player_cpu.cpu_move
+
+    expect(move).to eq(player_cpu.board.cells.keys.find {|key| key == move})
+  end
+
+  it "Test 5: can loose a game" do
+    player_1 = Player.new
+    player_cpu = Player.new(true)
+
+    expect(player_1.has_lost?).to eq(false)
+    3.times {player_1.cruiser.hit}
+    expect(player_1.has_lost?).to eq(false)
+    2.times {player_1.submarine.hit}
+    expect(player_1.has_lost?).to eq(true)
+  end
+end
